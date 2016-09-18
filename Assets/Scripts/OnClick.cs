f1be7b9808842826677d53840ea04e83d07047d8
Moritz Unseld <moritz.unseld@googlemail.com>
2016-09-18 23:08:16 +0200

    Fixed settings canvas scaling problem

--- Assets/Scripts/OnClick.cs
+++ Assets/Scripts/OnClick.cs
@@ -3,21 +3,14 @@ using System.Collections;
 
 public class OnClick : MonoBehaviour {
 
-    private AudioSource source;
+    
 
-    void Awake()
-    {
-        source = GetComponent<AudioSource>();
-    }
+    
 
     public void LoadScene(int level)
     {
         Application.LoadLevel(level);
     }
 
-    public void ChangeMusic(AudioClip music)
-    {
-        source.clip = music;
-        source.Play();
-    }
+    
 }
